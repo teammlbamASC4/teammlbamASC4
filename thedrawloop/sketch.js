712761089968cc6ead2b56f98f6e93fab7b708b7
Victor Ramirez <me@isvictorious.com>
2017-08-05 14:12:05 -0400

    updating local mac

--- thedrawloop/sketch.js
+++ thedrawloop/sketch.js
@@ -1,78 +1,64 @@
-//This is a simple one ball bousing program
-//Following are the features:
-//1.One ball bounces of left,top and right edges of canvas
-//2.The ball does not bounce of the bottom edge automatically
-//3.Ball bounces of the handle stuck to the bottom edge
-//4. Ball speed increases after every reflection
+// must setup global variables OUTSIDE of function scope
 
+//Making board a 2D nested array
+// 0 means blank, 1 means ship
 
-var xpos = 255; // set position of ball x
-var ypos = 25; // set position of ball y
-var xspeed = 2; // set initial speed of ball
-var yspeed = 2;// set initial speed of bally
-var xposhand; // declare a position for the  paddel
+var boxSize = 60;
 
-function setup() {
-    createCanvas(500, 500);
-    noStroke();
-    fill(random(255), random(255), random(255));
-    rectMode(CENTER);
+var board = [
+	[0,0,0,0,0],
+	[0,0,0,0,0],
+	[0,0,0,0,0],
+	[0,0,0,0,0],
+	[0,0,0,0,0]
+];
+
+var spot;
+function rand(num){
+	//takes a number argument and returns a random whole number between 0 and the argument
+	return Math.floor(Math.random()*num);
 }
 
-function draw() {
-    background(240, 95);
-    ellipse(xpos, ypos, 50, 50); //ball
-
-    //making sure handle does not leave canvas area
-
-    if (mouseX >= 40 && mouseX <= width - 40) {
-        xposhand = mouseX;
-    } else if (mouseX < 40) {
-        xposhand = 40;
-    } else if (mouseX > width - 40) {
-        xposhand = width - 40;
-    }
-    rect(xposhand, height - 2.5, 80, 5);
-
-    //Making the ball move
-    xpos += xspeed;
-    ypos += yspeed;
-
-    //Making the ball bounce out of left,top and right edges
-    if (xpos <= 25 || xpos >= width - 25) {
-        if (xspeed < 10 && xspeed > -10) { //controlling the speed
-            xspeed = xspeed * (-1.2);
-        } else {
-            xspeed = xspeed * (-1.01);
-        }
-    }
-    if (ypos <= 25) {
-        if (yspeed < 10 && yspeed > -10) {
-            yspeed = yspeed * (-1.2);
-        } else {
-            yspeed = yspeed * (-1.01);
-        }
-    }
-    //Making the ball bounce out of bottom handle
-    if (ypos >= height - 25) {
-        if (xpos <= xposhand + 65 && xpos >= xposhand - 65) {
-            if (yspeed < 10 && yspeed > -10) {
-                yspeed = yspeed * (-1.2);
-            } else {
-                yspeed = yspeed * (-1.01);
-            }
-        } else {
-            textAlign(CENTER);
-            textFont("Open Sans");
-            textStyle(BOLD);
-            text("GAME OVER", width / 2, height / 2);
-            noLoop();
-        }
-    }
+function setup(){
+	createCanvas(boxSize*5,boxSize*5);
+	// randomly assign 1 spot to be a ship
+	spot = {
+		"x":rand(5),
+		"y":rand(5)
+	}
 }
 
-//Change the colors on a mouse press event
-function mousePressed() {
-    fill(random(255), random(255), random(255));
+function draw(){
+	// redrawing background "clears" old content
+	background(0);
+
+	// redrawing current board
+	for(var i = 0; i < 5; i++){
+		//for each array in board
+		for(var k = 0; k <5; k++){
+			//each item in array
+			if(board[i][k]!=0){
+				//not a blank, a.k.a. spot with a ship
+				if(board[i][k]==1){
+					fill(225,0,0); //will change color of drawn square
+				}else{
+					fill(0,0,225); //will change color of drawn square
+				}
+			}else{
+				//blank
+				fill(225,225,225);
+			}
+			//position in board array reflected in x, y coordinate by multiplying index for row and column
+			rect(boxSize*i, boxSize*k, boxSize,boxSize);Ω
+		}
+	}
+	
 }
 
+function mouseClicked(){
+	if(spot.x==floor(mouseX/boxSize) && spot.y==floor(mouseY/boxSize)){
+		board[floor(mouseX/boxSize)][floor(mouseY/boxSize)]=1; //hit
+	}else{
+		board[floor(mouseX/boxSize)][floor(mouseY/boxSize)]=2; //miss
+	}
+}
